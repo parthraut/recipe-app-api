2537e1f0995a3dbe08632890fb57c4ef4fbca0b7
Parth Raut <praut@umich.edu>
2023-05-27 18:59:43 -0400

    added calcs

---
+++ app/app/test.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together."""
+        self.assertEqual(calc.add(5, 6), 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned."""
+        self.assertEqual(calc.subtract(11, 5), -6)
